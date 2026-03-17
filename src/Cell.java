6fd5f6855c6784e21669aa9b772249a443327a4f
danielmenachem <daniellemenachem@gmail.com>
2026-03-17 11:42:55 +0200

    Added Cell class

---
+++ src/Cell.java
@@ -0,0 +1,14 @@
+public class Cell {
+    public final int row;
+    public final int col; 
+
+    public Cell (int row, int col) {
+        this.row = row;
+        this.col = col; 
+    }
+
+    public String toString() {
+        return "(" + row + "," + col + ")"; 
+    }
+    
+}
